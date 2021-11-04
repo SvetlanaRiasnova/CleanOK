0bfa3318d733cfdb1e5b14785d12677267514cb8
SvetlanaRiasnova <R_S_U@outlook.com>
2021-11-04 17:28:32 +1000

    edit js for float nav menu

--- js/nav-float.js
+++ js/nav-float.js
@@ -1,7 +1,18 @@
-const floatNav=document.querySelector(".header__container-float")
+// const floatNav=document.querySelector(".header__container-float");
 
-function showNav() {
-    floatNav.classList.remove("hidden");
-}
+// function showNav() {
+//     if (document.scrollY + 100 +'px');
+//     floatNav.classList.remove("hidden");
+// }
 
-window.addEventListener('scroll', showNav);
+// window.addEventListener('scroll', showNav);
+
+var floatNav = document.querySelector('.header__container-float');
+window.addEventListener('scroll', function () {
+	var st = document.documentElement.scrollTop;
+  if (st > 60) {
+  	floatNav.classList.remove('hidden');
+  } else {
+    floatNav.classList.add('hidden');
+  }
+});
