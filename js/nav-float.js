38b4726cbbc8db17f81dc3d2a17fcbddf401776d
SvetlanaRiasnova <R_S_U@outlook.com>
2021-11-05 13:05:16 +1000

    add map & search block, edit btn

--- js/nav-float.js
+++ js/nav-float.js
@@ -1,18 +1,10 @@
-// const floatNav=document.querySelector(".header__container-float");
-
-// function showNav() {
-//     if (document.scrollY + 100 +'px');
-//     floatNav.classList.remove("hidden");
-// }
-
-// window.addEventListener('scroll', showNav);
-
 var floatNav = document.querySelector('.header__container-float');
 window.addEventListener('scroll', function () {
-	var st = document.documentElement.scrollTop;
-  if (st > 60) {
-  	floatNav.classList.remove('hidden');
-  } else {
-    floatNav.classList.add('hidden');
-  }
+	var scrollCount = document.documentElement.scrollTop;
+    if (scrollCount > 60) {
+        floatNav.classList.remove('hidden');
+    } else {
+        floatNav.classList.add('hidden');
+    }
+    behavior: 'smooth';
 });
