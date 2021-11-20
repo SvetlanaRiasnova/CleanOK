61c132d32afcff582066f39adaf51d42931a0f99
SvetlanaRiasnova <R_S_U@outlook.com>
2021-11-20 22:16:59 +1000

    textpage edit

---
+++ js/table.js
@@ -0,0 +1,7 @@
+// // let evenStringColor ;
+// let gridArr = document.getElementsByClassName('table-form');
+// console.log([gridArr]);
+//  for (let w of gridArr) {
+//      w.classList.add('blue');
+//      w = w+2;
+//  }
