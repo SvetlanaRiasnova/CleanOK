0246694d95643809111fac7c5cb26ec41c76ef60
SvetlanaRiasnova <R_S_U@outlook.com>
2021-11-21 21:15:20 +1000

    validation for action form

--- js/table.js
+++
@@ -1,7 +0,0 @@
-// // let evenStringColor ;
-// let gridArr = document.getElementsByClassName('table-form');
-// console.log([gridArr]);
-//  for (let w of gridArr) {
-//      w.classList.add('blue');
-//      w = w+2;
-//  }
