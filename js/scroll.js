4e42e7da05ff1e1c0567cf92dba8e788b3f7565d
SvetlanaRiasnova <R_S_U@outlook.com>
2022-01-05 00:07:00 +1000

    media edit and map searche created

--- js/scroll.js
+++ js/scroll.js
@@ -1,18 +1,4 @@
 
-// const anchors = document.querySelectorAll('a[href*="#"]');
-// for (let anchor of anchors) {
-//   anchor.addEventListener('click', function (e) {
-//     e.preventDefault();
-    
-//     const blockID = anchor.getAttribute('href').substr(1);
-    
-//     document.getElementById(blockID).scrollIntoView({
-//       behavior: 'smooth',
-//       block: 'start'
-//     });
-//   });
-// }
-
 document.querySelectorAll('a[href*="#"]').forEach(link => {
     link.addEventListener("click", function(e) {
         e.preventDefault();
