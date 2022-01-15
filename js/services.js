2e8384dc38f4082389a2d9f69653a283c62b1970
SvetlanaRiasnova <R_S_U@outlook.com>
2022-01-15 13:59:49 +1000

    removed onclick from html

--- js/services.js
+++ js/services.js
@@ -12,6 +12,7 @@ function openService(evt, service) {
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
+    tablinks[i].addEventListener('click', openService);
   }
 
   // Show the current tab, and add an "active" class to the link that opened the tab
